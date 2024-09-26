2c681d164ec045cf27c0c3671ac3af6fe0aed3e2
Angie Melissa <124700371+angiemeelissa@users.noreply.github.com>
2024-09-25 21:43:10 -0600

    Update Mercado de Acciones.ts

--- Mercado de Acciones.ts
+++ Mercado de Acciones.ts
@@ -1,7 +1,14 @@
-// ANGIE MELISSA SANTIAGO RODRIGUEZ
+class Order {
+    constructor(
+        public company: string,
+        public quantity: number,
+        public price: number,
+        public type: 'buy' | 'sell'  
+    ) {}
+}
 
 class MaxHeap {
-    public heap: number[];
+    public heap: Order[];
     private n: number;
 
     constructor(size: number) {
@@ -9,7 +16,7 @@ class MaxHeap {
         this.n = 0;
     }
 
-    public CheckMax(): number {
+    public CheckMax(): Order {
         return this.heap[1];
     }
 
@@ -21,7 +28,7 @@ class MaxHeap {
         return this.n;
     }
 
-    public Insert(value: number): void {
+    public Insert(value: Order): void {
         if (this.n === this.heap.length - 1) {
             this.Resize(2 * this.heap.length);
         }
@@ -32,17 +39,17 @@ class MaxHeap {
 
     private BubbleUp(i: number): void {
         let parent = Math.floor(i / 2);
-        while (i > 1 && this.heap[parent] < this.heap[i]) {
+        while (i > 1 && this.heap[parent].price < this.heap[i].price) {
             [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
             i = parent;
             parent = Math.floor(i / 2);
         }
     }
 
-    public GetMax(): number {
+    public GetMax(): Order {
         let max = this.heap[1];
         this.heap[1] = this.heap[this.n];
-        this.heap[this.n] = 0;
+        this.heap[this.n] = undefined as any;
         this.n--;
         this.Sink(1);
         return max;
@@ -51,8 +58,8 @@ class MaxHeap {
     private Sink(i: number): void {
         while (2 * i <= this.n) {
             let j = 2 * i;
-            if (j < this.n && this.heap[j] < this.heap[j + 1]) j++;
-            if (this.heap[i] >= this.heap[j]) break;
+            if (j < this.n && this.heap[j].price < this.heap[j + 1].price) j++;
+            if (this.heap[i].price >= this.heap[j].price) break;
             [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
             i = j;
         }
@@ -67,9 +74,8 @@ class MaxHeap {
     }
 }
 
-
 class MinHeap {
-    private heap: number[];
+    private heap: Order[];
     private n: number;
 
     constructor(size: number) {
@@ -77,7 +83,7 @@ class MinHeap {
         this.n = 0;
     }
 
-    public CheckMin(): number {
+    public CheckMin(): Order {
         return this.heap[1];
     }
 
@@ -89,7 +95,7 @@ class MinHeap {
         return this.n;
     }
 
-    public Insert(value: number): void {
+    public Insert(value: Order): void {
         if (this.n === this.heap.length - 1) {
             this.Resize(2 * this.heap.length);
         }
@@ -100,17 +106,17 @@ class MinHeap {
 
     private BubbleUp(i: number): void {
         let parent = Math.floor(i / 2);
-        while (i > 1 && this.heap[parent] > this.heap[i]) {
+        while (i > 1 && this.heap[parent].price > this.heap[i].price) {
             [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
             i = parent;
             parent = Math.floor(i / 2);
         }
     }
 
-    public GetMin(): number {
+    public GetMin(): Order {
         let min = this.heap[1];
         this.heap[1] = this.heap[this.n];
-        this.heap[this.n] = 0;
+        this.heap[this.n] = undefined as any;
         this.n--;
         this.Sink(1);
         return min;
@@ -119,8 +125,8 @@ class MinHeap {
     private Sink(i: number): void {
         while (2 * i <= this.n) {
             let j = 2 * i;
-            if (j < this.n && this.heap[j] > this.heap[j + 1]) j++;
-            if (this.heap[i] <= this.heap[j]) break;
+            if (j < this.n && this.heap[j].price > this.heap[j + 1].price) j++;
+            if (this.heap[i].price <= this.heap[j].price) break;
             [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
             i = j;
         }
@@ -133,13 +139,62 @@ class MinHeap {
         }
         this.heap = newHeap;
     }
+}
 
-    public Print(): void {
-        let tree = "";
-        for (let i = 1; i <= this.n; i++) {
-            tree += "[" + this.heap[i] + "] ";
+class StockMarketSimulator {
+    private buyOrders: MaxHeap;
+    private sellOrders: MinHeap;
+    private transactionHistory: string[] = [];
+
+    constructor() {
+        this.buyOrders = new MaxHeap(10);
+        this.sellOrders = new MinHeap(10);
+    }
+
+    public AddOrder(order: Order): void {
+        if (order.type === 'buy') {
+            this.buyOrders.Insert(order);
+        } else {
+            this.sellOrders.Insert(order);
         }
-        console.log(tree);
+        this.MatchOrders();
+    }
+
+    private MatchOrders(): void {
+        while (!this.buyOrders.Empty() && !this.sellOrders.Empty()) {
+            let highestBuyOrder = this.buyOrders.CheckMax();
+            let lowestSellOrder = this.sellOrders.CheckMin();
+
+            if (highestBuyOrder.price >= lowestSellOrder.price) {
+                let quantityMatched = Math.min(highestBuyOrder.quantity, lowestSellOrder.quantity);
+
+                highestBuyOrder.quantity -= quantityMatched;
+                lowestSellOrder.quantity -= quantityMatched;
+
+                this.LogTransaction(highestBuyOrder.company, quantityMatched, lowestSellOrder.price);
+                if (highestBuyOrder.quantity === 0) {
+                    this.buyOrders.GetMax();
+                }
+                if (lowestSellOrder.quantity === 0) {
+                    this.sellOrders.GetMin();
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    private LogTransaction(company: string, quantity: number, price: number): void {
+        let transaction = `Compañía: ${company}, Acciones: ${quantity}, Precio: ${price}`;
+        this.transactionHistory.push(transaction);
+    }
+
+    public ShowTransactionHistory(): void {
+        console.log('Historial de Transacciones:', this.transactionHistory);
     }
 }
 
+
+const simulator = new StockMarketSimulator();
+simulator.AddOrder(new Order('Apple', 50, 140, 'sell')); 
+simulator.ShowTransactionHistory();
